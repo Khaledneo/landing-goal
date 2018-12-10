0ccef23b6f1e1a187cc7aa1ce2b2904cb514def2
khaledneo <khaled@neo.ae>
2018-12-10 09:30:41 +0200

    Created the base container and starting with the project structure

---
+++ src/containers/goal-calculator.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Aux from "../Hoc/Aux";
+
+class Goal extends Component {
+    render(){
+        return (
+            <Aux>
+                <div>Hello world</div>
+            </Aux>
+        )
+    }
+};
+
+export default Goal;
