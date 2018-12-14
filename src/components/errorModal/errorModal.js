7b0f2b961e5762df4eef066bdaca2dc741d95d0b
khaledneo <khaled@neo.ae>
2018-12-14 13:32:05 +0200

    Working on validation

--- src/components/errorModal/errorModal.js
+++ src/components/errorModal/errorModal.js
@@ -1,21 +1,27 @@
 import React from "react";
 import Aux from "../../Hoc/Aux";
+import "./errorModal.scss";
 
 const errorModal = (props) => {
     return (
-        <div className="modal fade" id="errorModal" tabIndex="-1" role="dialog" aria-labelledby="errorModal"
-            aria-hidden="true">
-            <div className="modal-dialog modal-dialog-centered" role="document">
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <h5 className="modal-title" id="exampleModalLongTitle">Error</h5>
-                    </div>
-                    <div className="modal-body">
-                        {props.errorMessage}
+        <Aux>
+            <div className="modal fade" id="errorModal" tabIndex="-1" role="dialog" aria-labelledby="errorModal"
+                aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLongTitle">Error!</h5>
+                        </div>
+                        <div className="modal-body">
+                            {props.errorMessage}
+                        </div>
+                        <div className="modal-footer">
+                            <button className="btn btn-danger"  data-dismiss="modal" aria-label="Close">Close</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </Aux>
     );
 };
 
