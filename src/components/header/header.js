197a1d2e123900f7f07f911e705e1b1c281dd493
Khaled Ramadan <khaled.ra@live.com>
2018-12-20 22:45:16 +0200

    Linked the header and the footer pages

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -9,22 +9,22 @@ const leftMenu = [
     {
         id: "wsw",
         title: "Why SmartWealth",
-        url:"nbkcapitalsmartwealth.com/why-smart-wealth"
+        url:"https://nbkcapitalsmartwealth.com/why-smart-wealth"
     },
     {
         id: "pricing",
         title: "Pricing",
-        url:"nbkcapitalsmartwealth.com/why-smart-wealth"
+        url:"https://nbkcapitalsmartwealth.com/pricing"
     },
     {
         id: "learn",
         title: "Learn",
-        url:"nbkcapitalsmartwealth.com/why-smart-wealth"
+        url:"https://nbkcapitalsmartwealth.com/learn"
     },
     {
         id: "help",
         title: "Help",
-        url:"nbkcapitalsmartwealth.com/why-smart-wealth"
+        url:"http://help.nbkcapitalsmartwealth.com/"
     }
 ];
 
@@ -32,17 +32,17 @@ const rightMenu = [
     {
         id: "localization",
         title: "عربي",
-        url:"nbkcapitalsmartwealth.com/why-smart-wealth"
+        url:"/"
     },
     {
         id: "login",
         title: "Login",
-        url:"nbkcapitalsmartwealth.com/why-smart-wealth"
+        url:"https://my.nbkcapitalsmartwealth.com/"
     },
     {
         id: "get_started",
         title: "Get started",
-        url:"nbkcapitalsmartwealth.com/why-smart-wealth"
+        url:"https://my.nbkcapitalsmartwealth.com/questionnaire-1"
     },
 ];
 
@@ -50,7 +50,7 @@ const getLeftMenu = () => {
     return (
     <ul className="navbar-nav mr-auto">
         {leftMenu.map((page)=> {
-                return  <li className="nav-item" key={page.id}><a className="nav-link" href={[page.url]}>{page.title}</a></li>
+                return  <li className="nav-item" key={page.id}><a className="nav-link" href={page.url}>{page.title}</a></li>
         })}
     </ul>
     )
@@ -71,7 +71,7 @@ const header = () => {
         <Aux>
            <nav className="navbar-light navbar navbar-expand-md ">
                 <div className="container">
-                    <a className="navbar-brand" href="nbkcapitalsmartwealth.com">
+                    <a className="navbar-brand" href="/">
                         <img className="logo" src={logo} alt="logo"/>
                     </a>
                     <button className="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
